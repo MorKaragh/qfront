5eee9911745e38616c89ece850e09988fb47a01e
Sergei <hentai.exe@gmail.com>
2023-03-01 23:28:17 +0300

    zzzz

--- src/qfront/features/identity/identity_plank.cljs
+++ src/qfront/features/identity/identity_plank.cljs
@@ -2,6 +2,14 @@
   (:require [clojure.string :as str]
             [re-frame.core :as rf]))
 
+(defn avatar-url [user]
+  (let [url "http://localhost:3001/"
+        id (:id user)]
+   (case (:sex user)
+     "female" (str url "pixel-f?seed=" id)
+     "male" (str url "pixel-m?seed=" id)
+     :else (str url "cat?seed=" id))))
+
 (defn single-user-plank [user]
   (let [selected-ids @(rf/subscribe [:selected-ids])]
     [:div {:class (if (contains?  selected-ids (:id user))
@@ -9,7 +17,7 @@
                     "user-profile ")
            :on-click #(rf/dispatch [:plank-click (:id user)])}
     ;;  [:img {:src (:avatar user) :class "user-avatar"}]
-     [:img {:src (str "http://localhost:3001/?seed=" (:id user)) :class "user-avatar"}]
+     [:img {:src (avatar-url user) :class "user-avatar"}]
      [:div {:class "user-description"}
       [:h4 {:class "user-name"} (str "Name: " (:name user))]
       [:ul {:class "user-attributes"}
