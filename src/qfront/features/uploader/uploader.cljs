01ce6afdd9c6d745217de760fe4f95447d6d4347
Sergei <hentai.exe@gmail.com>
2023-04-25 22:34:53 +0300

    upl

--- src/qfront/features/uploader/uploader.cljs
+++ src/qfront/features/uploader/uploader.cljs
@@ -1,16 +1,21 @@
-(ns qfront.features.uploader.uploader 
+(ns qfront.features.uploader.uploader
+  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [reagent.core :as r]
-            [ajax.core :as ajax]
             [re-frame.core :as rf]
             [cljs-http.client :as http]
             [cljs.core.async :refer [<! >! chan alts!]]))
 
-(defn- upload-file [file]
-  (http/post "http://localhost:3001/images" 
-             {:with-credentials? false
-              :multipart-params [["mykey" "myval"] ["uplfile" file]]}))
+(defn after-upload [hash])
 
-(defn- handle-drag [active] 
+(defn- upload-file [file]
+  (go (let [response
+            (<! (http/post "http://localhost:3001/images"
+                        {:with-credentials? false
+                         :multipart-params [["mykey" "myval"] ["uplfile" @file]]}))]
+        (prn response)
+        (reset! file nil))))
+
+(defn- handle-drag [active]
   (fn [x]
     (.preventDefault x)
     (.stopPropagation x)
@@ -26,35 +31,35 @@
       (prn "recieved-file: " recieved-file)
       (reset! active false)
       (reset! file (-> x .-dataTransfer .-files (aget 0)))
-      (prn "FILE:" @file)))
-;;   (let [files (.-files (.-dataTransfer x))] (prn (aget files 0)))
-  )
+      (prn "FILE:" @file))))
 
 (defn- on-btn-click [file]
   (fn [e]
     (.preventDefault e)
     (prn "uploading!")
-    (upload-file @file)))  
+    (upload-file file)))
 
 (defn file-upload []
-    (let [active (r/atom false)
-          file-input (r/atom nil)]
-     (fn []
-       [:form#form-file-upload {:on-drag-enter (handle-drag active)}
-        [:input {:id        "input-file-upload"
-                 :type      "file"
-                 :multiple  true
-                 :on-change #(reset! file-input (-> % .-target .-files (aget 0)))
-                 }]
-        [:label#label-file-upload {:html-for "input-file-upload"
-                                   :class    (when @active "drag-active")}
-         [:div 
-          [:p (if (nil? @file-input) 
-                "Drag and Drop"
-                (.-name @file-input))]
-          [:button {:on-click (on-btn-click file-input)} "Upload a file!"]]]
-        (when @active [:div#drag-file-element 
-                       {:on-drag-enter (handle-drag active)
-                        :on-drag-leave (handle-drag active)
-                        :on-drag-over  (handle-drag active)
-                        :on-drop       (handle-drop active file-input)}])])))
+  (let [active (r/atom false)
+        file-input (r/atom nil)
+        id (random-uuid)]
+    (fn []
+      [:form.form-file-upload {:on-drag-enter (handle-drag active)}
+       [:input.input-file-upload {:id id
+                                  :type      "file"
+                                  :multiple  true
+                                  :accept ".jpg,.jpeg,.png,.tiff,.gif"
+                                  :on-change #(reset! file-input (-> % .-target .-files (aget 0)))}]
+       [:label.label-file-upload {:html-for id
+                                  :class    (when @active "drag-active")}
+        [:div
+         [:p (if (nil? @file-input)
+               "Выберите файл"
+               (.-name @file-input))]
+         (when (not (nil? @file-input)) 
+           [:button.upload-button {:on-click (on-btn-click file-input)} "Загрузить!"])]]
+       (when @active [:div.drag-file-element
+                      {:on-drag-enter (handle-drag active)
+                       :on-drag-leave (handle-drag active)
+                       :on-drag-over  (handle-drag active)
+                       :on-drop       (handle-drop active file-input)}])])))
